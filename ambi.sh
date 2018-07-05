0a393f014e2b24363726246384c281436dda4d05
Alexvanaxe <alexvanaxe@gmail.com>
2018-07-05 16:24:37 -0300

    ambi ip updated

--- ambi.sh
+++ ambi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source ./activate
-ng serve --configuration=working --disable-host-check  --host 192.168.15.3
+ng serve --configuration=working --disable-host-check  --host 192.168.15.8
