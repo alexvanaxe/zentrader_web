2930cff5d45e205f02a48ef0e8de7d2d9542b0be
noname <teste123@tgefdsa.com>
2018-11-26 15:39:02 -0200

    More infos for the stocks

--- src/app/stock/model/resume.ts
+++ src/app/stock/model/resume.ts
@@ -2,6 +2,10 @@ export class Resume {
   constructor(
     public code: string = '',
     public price: string = null,
+    public name: string = null,
+    public sector: string = null,
+    public subsector: string = null,
+
     public owned: string = null,
     public average_price: string = null,
     public stock_value: string = null,
--- src/app/stock/model/stock.ts
+++ src/app/stock/model/stock.ts
@@ -3,6 +3,9 @@ export class Stock {
     public pk: string = null,
     public code: string = '',
     public price: string = null,
+    public name: string = null,
+    public sector: string = null,
+    public subsector: string = null,
 
     public owned: string = null
   ) {  }
