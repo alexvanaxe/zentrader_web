95387e07b38698364e9817fbdefd11b6bc42d330
noname <teste123@tgefdsa.com>
2018-12-18 15:27:02 -0200

    Added color to negative numbers

---
+++ src/app/shared/number-color.pipe.ts
@@ -0,0 +1,29 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Pipe({
+  name: 'numberColor'
+})
+export class NumberColorPipe implements PipeTransform {
+
+  constructor(private domSanitizer: DomSanitizer) {
+
+  }
+
+  transform(value: number, args?: any): any {
+    var color = "black";
+    var money = parseFloat(value);
+    if (money < 0) {  
+      color = "red";
+    } else if (money > 0) {
+      color = "blue";
+    }
+
+    if (value == null) {
+      value = "NA";
+    }
+    var htmlColored = `<span style='color: ${color}'>${value}</span>` 
+    return this.domSanitizer.bypassSecurityTrustHtml(htmlColored);
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { IrbrModule } from '../irbr/irbr.module';
+import { NumberColorPipe } from './number-color.pipe';
 
 @NgModule({
   imports: [
     CommonModule,
     IrbrModule
   ],
-  declarations: [MenuComponent],
+  declarations: [MenuComponent, NumberColorPipe],
   providers: [MenuComponent],
-  exports: [MenuComponent]
+  exports: [MenuComponent, NumberColorPipe]
 })
 export class SharedModule { }
