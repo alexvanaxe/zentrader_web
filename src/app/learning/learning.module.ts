7878a84ee99219e61dc2f9d0b6d5df54408a75bf
Alexvanaxe <alexvanaxe@gmail.com>
2018-12-29 23:11:42 -0200

    Paper Buy Getting ready to be shown.

--- src/app/learning/learning.module.ts
+++ src/app/learning/learning.module.ts
@@ -4,10 +4,12 @@ import { PaperBuyCardComponent } from './paper-buy-card/paper-buy-card.component
 import { PaperBuyService } from './paper-buy.service';
 import { LearningComponent } from './learning/learning.component';
 import { SharedModule } from '../shared/shared.module';
+import { StockModule } from '../stock/stock.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    StockModule,
     SharedModule
   ],
   declarations: [PaperBuyCardComponent, LearningComponent],
