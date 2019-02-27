b1483ae22f01c04d266a5efb400b0e7cd9782787
noname <teste123@tgefdsa.com>
2019-02-27 14:06:44 -0300

    Complete the preoficial welcome page

---
+++ src/app/zen-auth/welcome-page/welcome-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'zen-welcome-page',
+  templateUrl: './welcome-page.component.html',
+  styleUrls: ['./welcome-page.component.css']
+})
+export class WelcomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/zen-auth/zen-auth.module.ts
+++ src/app/zen-auth/zen-auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { ZentraderAuthComponent } from './zentrader-auth/zentrader-auth.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
-  declarations: [ZentraderAuthComponent],
+  declarations: [ZentraderAuthComponent, WelcomePageComponent],
   imports: [
     CommonModule,
     FormsModule
