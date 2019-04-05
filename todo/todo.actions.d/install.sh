f28cc1e691bd3393e8a376a9a1883c76cc64bc2b
noname <teste123@tgefdsa.com>
2019-04-05 16:20:06 -0300

    Todos atualizados.

--- todo/todo.actions.d/install.sh
+++ todo/todo.actions.d/install.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
 git clone https://github.com/rebeccamorgan/due.git
+wget https://raw.githubusercontent.com/mgarrido/todo.txt-cli/note/todo.actions.d/archive; chmod 755 ./archive
+wget https://raw.githubusercontent.com/mgarrido/todo.txt-cli/note/todo.actions.d/del; chmod 755 ./del
+wget https://raw.githubusercontent.com/mgarrido/todo.txt-cli/note/todo.actions.d/note; chmod 755 ./note
+wget https://raw.githubusercontent.com/mgarrido/todo.txt-cli/note/todo.actions.d/rm; chmod 755 ./rm
