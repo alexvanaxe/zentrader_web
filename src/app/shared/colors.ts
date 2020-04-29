a1f9cb83a3dc3458272e2426ec07d51bb04346d5
Alexvanaxe <alexvanaxe@gmail.com>
2020-04-29 16:00:11 -0300

    Including new files for colors

---
+++ src/app/shared/colors.ts
@@ -0,0 +1,4 @@
+export class Colors {
+    public static readonly gain: string = '#d7e2c2';
+    public static readonly loose: string = '#D2CACA';
+}
---
+++ src/environments/environment.js
@@ -0,0 +1,18 @@
+"use strict";
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+exports.__esModule = true;
+exports.environment = {
+    production: false,
+    backend_api: 'http://localhost:8000/',
+    clientSecret: 'gYaJJjKYUVDAjrjRcEVNLqReClSvS5F7ND90Fc1sF113emaYqy0Z6vnhSla6olt0UR3x0whcDnrM1YXb755fqNNal4f6sK9pIMr4UfRZIKXrahmAhIq6Bq0KKJgU8s4Y',
+    clientId: '7zCrOkq31oPgDBJFkrD37HJS64c7Btjg1vdJDKXM'
+};
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
