4a9c851c789a6fa34dd85cdc761929200d1788c4
Alexvanaxe <alexvanaxe@gmail.com>
2020-07-01 15:20:22 -0300

    Updating design of the site

--- src/app/zen-auth/welcome-page/welcome-page.component.ts
+++ src/app/zen-auth/welcome-page/welcome-page.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject, ViewEncapsulation } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'zen-welcome-page',
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.css']
+  styleUrls: ['./welcome-page.component.css'],
+  encapsulation: ViewEncapsulation.None
+  
 })
-export class WelcomePageComponent implements OnInit {
+export class WelcomePageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private _document) { }
 
   ngOnInit() {
+    this._document.body.classList.add('bg');
+  }
+
+  ngOnDestroy() {
+    this._document.body.classList.remove('bg');
   }
 
 }
