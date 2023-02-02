d0d4c7da02eef0bcc4212a4256f9aba0899c7526
AVA <alexvanaxe@gmail.com>
2023-02-02 20:30:32 -0300

    Fix the http url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backend_api: 'https://192.168.0.101:8001/',
+  backend_api: 'http://192.168.0.101:8001/',
   clientSecret: 'pbkdf2_sha256$390000$j1SBW1iU9GNcbgpCGwrmIV$ed3pP5Pf+JsIDM9a2z4tQmQ20GgiZS/Dx6kbLuPNupI=',
   clientId: 'vBlSlyuLw4ABnFgU4wQsiUKGG4JEEY2048mCEaoU'
 }
